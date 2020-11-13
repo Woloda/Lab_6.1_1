ca8f73c1a0939346417645452202cd5ded23fd51
Woloda <dorozovecvoloda@gmail.com>
2020-11-13 15:42:54 +0200

    Lab__6.1_1

---
+++ Lab_6.1(1)/Lab_6.1(1).cpp
@@ -0,0 +1,87 @@
+﻿// 1 Спосіб --- "Ітераційний"
+#include <iostream>
+#include <time.h>
+#include <iomanip>
+#include <Windows.h>
+
+void value_elements(int* b, const int elements, int Low, int High); // Формування масиву.
+void vuvestu(int* b, const int elements);                           // Вивід масиву.
+int ymova(int* b, const int elements);                              // Модифікація масиву за вказаним критерієм.
+int suma(int* b, const int elements);                               // Сума елементів модифікованого масиву.                        
+
+int main(void)
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	srand((unsigned)time(NULL));
+	const int elements = 22;
+	int b[elements];
+
+	int Low = -40;
+	int High = 50;
+
+	value_elements(b, elements, Low, High);
+
+	std::cout << "сформований масив: " << std::endl;
+	std::cout << std::endl;
+	vuvestu(b, elements);
+
+	int kilkist = ymova(b, elements);
+
+	std::cout << "Модифікований масив: " << std::endl;
+	std::cout << std::endl;
+	vuvestu(b, elements);
+
+	int value = suma(b, elements);
+
+	std::cout << "Сума елементів які задовольняють вказаному критерію: " << value << std::endl;
+	std::cout << std::endl;
+	std::cout << "Кількість елементів які задовольняють вказаному критерію: " << kilkist << std::endl;
+
+	return 0;
+}
+
+void value_elements(int* b, const int elements, int Low, int High)
+{
+	int p_index = 0;
+	for (int index = p_index; index < elements; index++)
+	{
+		b[index] = Low + rand() % (High - Low + 1);
+	}
+}
+
+void vuvestu(int* b, const int elements)
+{
+	int p_index = 0;
+	for (int index = p_index; index < elements; index++)
+		std::cout << std::setw(5) << b[index];
+	std::cout << std::endl;
+	std::cout << std::endl;
+}
+
+int ymova(int* b, const int elements)
+{
+	int sum = 0;
+	int p_index = 0;
+	for (int index = p_index; index < elements; index++)
+	{
+		if (b[index] % 5 || b[index] == 0)
+			sum += 1;
+		else
+			b[index] = 0;
+	}
+
+	return sum;
+}
+
+int suma(int* b, const int elements)
+{
+	int p_index = 0;
+	int sum = 0;
+	for (int index = p_index; index < elements; index++)
+	{
+		sum += b[index];
+	}
+	return sum;
+}
---
+++ UnitTest(Lab_6.1(1))/UnitTest(Lab_6.1(1)).cpp
@@ -0,0 +1,27 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Lab_6.1(1)/Lab_6.1(1).cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTestLab611
+{
+	TEST_CLASS(UnitTestLab611)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			int masuv_b[22] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22 };
+			ymova(masuv_b, 22);
+			int sum_elem = suma(masuv_b, 22);
+			Assert::AreEqual(sum_elem, 203);
+		}
+		TEST_METHOD(TestMethod2)
+		{
+			int masuv_b[22] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22 };
+			int sum_kil_elem = ymova(masuv_b, 22);
+			Assert::AreEqual(sum_kil_elem, 18);
+		}
+	};
+}
